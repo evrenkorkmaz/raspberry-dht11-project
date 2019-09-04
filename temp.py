56ffe076d7bac96358fe87aa08f2bb29d1875eb3
evren korkmaz <evrennkorkmaz@gmail.com>
2019-09-04 17:47:07 +0300

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,39 +1,39 @@
-import ipinfo
-import grovepi
-import time
-import smtplib
-dht_sensor_port=4
-dht_sensor_type=0
-import time
-import re
-data=open('data.txt','w')
-[ temp,hum ] = grovepi.dht(dht_sensor_port,dht_sensor_type)
-print ('Temp: '+ str(temp) + '*C' + '\tHumidity:' + ' %'+ str(hum) + ' ' + str(time.strftime("%s",time.gmtime())))
+# Author: Evren Korkmaz
+# This project read the value on DHT11 sensor and send the value to remove server da$
 
-access_token = '9d363e5856b384'
+import ipinfo     # locatiom lib
+import grovepi    # grove pi shield lib
+import smtplib    # stmp mail lib
+dht_sensor_port=4 # sensor port D4
+dht_sensor_type=0 # sensor type DHT11-> 0 DHT22-> 1
+data=open('data.txt','w') #open file for write
+[ temp,hum ] = grovepi.dht(dht_sensor_port,dht_sensor_type) # take value with grobep$
+                                                            # define the value integ$
+print ('Temp: '+ str(temp) + '*C' + '\tHumidity:' + ' %'+ str(hum)) # Print the valu$
+
+# this part for the location information
+access_token = '9d363e5856b384'          # token value from ipinfo.io
 handler = ipinfo.getHandler(access_token)
-ip_address = '185.136.56.2'
-details = handler.getDetails(ip_address)
+ip_address = '185.136.56.2'              # ip addres to this pc
+details = handler.getDetails(ip_address) #define the location value on details
 
-print details.org
 location=details.org
-location=location.replace(" ", "\ ")
-print location
+location=location.replace(" ", "\ ") # change the white spaceses to \ because influx$
 
-
+# write the value on data.txt
 data.write('temperature,location='+ location + ' temp='+ str(temp))
 data.write('\n')
 data.write('temperature,location='+ location + ' hum='+ str(hum))
 
 
-
+# mail function
 def mail(content):
     mail = smtplib.SMTP("smtp.gmail.com",587) 
     mail.starttls()
     mail.login('gozenintern@gmail.com','iskenHub123**')
     mail.sendmail("gozenintern@gmail.com","muhammetkrn19@gmail.com",content)
 
-
+# alert values
 if temp>=21.0: 
     content = 'Oda sicakligi ' + str(temp) + '*C' + ' Sicaklik yuksek!'
     mail(content)
