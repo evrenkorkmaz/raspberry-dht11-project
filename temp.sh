2bb7a0228cc9201a142780d913c15989cea4298c
evren korkmaz <evrennkorkmaz@gmail.com>
2019-09-05 10:05:01 +0300

    Update temp.sh

--- temp.sh
+++ temp.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 while true; do
-    python3 tempmail.py
-    curl -i -XPOST "http://35.198.129.164:8086/write?db=finally" --data-binary @data.txt
+    python3 temp.py # define python file 
+    # curl command for the send data on the data.txt file.
+    # First define a influxdb api address with 8086/tcp port( "http://xxx.xxx.xxx.xxx:8086/ ) 
+    # and add databse name  ( /write?db=DATABASE-NAME")
+    curl -i -XPOST "http://xxx.xxx.xxx.xxx:8086/write?db=db_name" --data-binary @data.txt
+    #loop time 
     sleep 5s
 done
 
